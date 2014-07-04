26912de6d0af7406060e67935435a9ef97b0174f
Björn Raupach <raupach.bjoern@googlemail.com>
2014-07-04 14:37:18 +0200

    Bootstrap 3.2

--- bootstrapify.jsf.js
+++ bootstrapify.jsf.js
@@ -1,6 +1,6 @@
-/* 
+/*
  * Copyright 2013 Björn Raupach
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -8,10 +8,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -20,7 +20,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-(function($) {
+(function($,jsf) {
 
     // credits: http://stackoverflow.com/questions/8584098/how-to-change-an-element-type-using-jquery#15554920
     $.fn.changeElementType = function(newType) {
@@ -58,11 +58,11 @@
         });
     }
 
-    // <h:messages id="messages" 
+    // <h:messages id="messages"
     //             globalOnly="true"
-    //             infoClass="alert-info" 
+    //             infoClass="alert-info"
     //             warnClass="alert-warn"
-    //             errorClass="alert-error" 
+    //             errorClass="alert-error"
     //             fatalClass="alert-success" />
     function bootstrapifyMessages() {
         $("#messages").changeElementType("div");
@@ -81,20 +81,52 @@
         $("table.input-radio").remove();
     }
 
-    // <h:message for="..." styleClass="help-inline" />
+    // <h:message for="..." styleClass="help-block" />
     function bootstrapifyMessage() {
-        $("form .help-inline").each(function() {
-            $(this).parents(".control-group").addClass("error");
-            $(this).siblings(".help-block").remove();
+        $("form .jsf-message").each(function() {
+            $(this).parents(".form-group").addClass("has-error");
+            $(this).removeClass("jsf-message").addClass("help-block");
+            //$(this).siblings(".help-block").remove();
         });
     }
 
-    // Call these methods onload
-    $(document).ready(function() {
+    // <:selectOneMenu ... />
+    function bootstrapifySelectOneMenu() {
+        $("select").removeAttr("size");
+    }
+
+    function bootstrapifyDatePicker() {
+        $(".input-group.date").datepicker({
+            language: "de",
+            todayHighlight: true
+        });
+    }
+
+    function bootstrapifyTooltip() {
+        $("[data-toggle='tooltip']").tooltip();
+    }
+
+    function bootstrapifyPopover() {
+        $("[data-toggle='popover']").popover();
+    }
+
+    function bootstrapifyAll() {
         bootstrapifyButtons();
         bootstrapifyMessages();
         bootstrapifyMessage();
         bootstrapifyInputRadio();
+        bootstrapifySelectOneMenu();
+        bootstrapifyDatePicker();
+        bootstrapifyTooltip();
+        bootstrapifyPopover();
+    }
+
+    // Call these methods on document ready
+    $(document).ready(function() {
+        bootstrapifyAll();
+        jsf.ajax.addOnEvent(bootstrapifyAll);
     });
 
-})(jQuery);
+
+
+})(jQuery,jsf);
